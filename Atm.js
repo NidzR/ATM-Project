4c47a00e2e6a35e1c10fc74b6b194ef9587682e1
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 15:06:12 +0500

    ATM CLI Project
    ATM. CLI PROJECT
    with node_modules
    package.json
    package-lock.json
    tsconfig.json
    npm init --y
    tsc --init
    npm i inquirer
    npm i @types/inquirer -D

---
+++ Atm.js
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "Kindly Enter your Id:"
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly Enter your PIN:"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving"],
+        message: "Select your account type:",
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdrawal"],
+        message: "Select your transaction",
+        when(answers) {
+            return answers.accountType === "Current";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        choices: [1000, 2000, 10000, 20000],
+        message: "Select your amount",
+        when(answers) {
+            return answers.transactionType === "Fast Cash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter the withdrawal amount:",
+        when(answers) {
+            return answers.transactionType === "Withdrawal";
+        }
+    }
+]);
+if (answers.userId && answers.userPin) {
+    const balance = Math.floor(Math.random() * 10000000);
+    console.log("Current balance:", balance);
+    const enteredAmount = answers.amount;
+    if (balance >= enteredAmount) {
+        console.log("Insufficient Balance");
+    }
+    else {
+        const remaining = balance - enteredAmount;
+        console.log("Your remaining balance is", remaining);
+    }
+}
